56005992ea38d2a05955625537f2e8605c5c5bbe
panbin <bpan@hpe.com>
2017-11-21 10:10:36 +0800

    remove package-lock and merge service.

--- server/service.js
+++ server/service.js
@@ -2,10 +2,29 @@ var express = require('express');
 var mongoClient = require('mongodb');
 var app = express();
 var DB_STR = 'mongodb://localhost:27017';
+var DB_CONN_STR = 'mongodb://shc-devops-master.hpeswlab.net:27017/failsafereports';
+
+
+var sendFailsafeReports = function(res) {
+  mongoClient.connect(DB_CONN_STR, function (err,db) {
+    db.collection('reports').find().limit(10)
+      .toArray(function (err, result){
+        res.header("Access-Control-Allow-Origin", "*");
+        res.set({'Content-Type':'text/json','Encodeing':'utf8'})
+        res.send(result);
+      });
+    db.close();
+  });
+}
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
+app.get('/api/failsafereports', function (req, res) {
+  sendFailsafeReports(res);
+});
+
 
 
 var server = app.listen(3000, function () {
