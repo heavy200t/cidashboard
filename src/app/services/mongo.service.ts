6281e861947ebd2878dee16b2cded94615a6a5fa
panbin <bpan@hpe.com>
2017-11-23 10:22:20 +0800

    For mock

--- src/app/services/mongo.service.ts
+++ src/app/services/mongo.service.ts
@@ -2,15 +2,21 @@ import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
 import {FailsafeReport} from '../data-model/failsafe-reports';
+import {of} from 'rxjs/observable/of';
+import {TESTDATA} from '../mock-data';
 
 @Injectable()
 export class MongoService {
 
   constructor(private http: HttpClient) { }
 
+  // getFailsafeReports(jobName: string, buildId: string): Observable<FailsafeReport[]> {
+  //   const url = `http://localhost:3000/api/failsafereports/${jobName}/${buildId}`;
+  //   return this.http.get<FailsafeReport[]>(url);
+  // }
+
   getFailsafeReports(jobName: string, buildId: string): Observable<FailsafeReport[]> {
-    const url = `http://localhost:3000/api/failsafereports/${jobName}/${buildId}`;
-    return this.http.get<FailsafeReport[]>(url);
+    return of(TESTDATA);
   }
 
 }
