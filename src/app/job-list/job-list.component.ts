d26cb228bcda4a384f506432f79714d43f670984
panbin <bpan@hpe.com>
2017-11-27 10:46:46 +0800

    Add joblist

---
+++ src/app/job-list/job-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {MongoService} from '../services/mongo.service';
+
+@Component({
+  selector: 'app-job-list',
+  templateUrl: './job-list.component.html',
+  styleUrls: ['./job-list.component.css']
+})
+export class JobListComponent implements OnInit {
+  jobs: String[];
+
+  constructor(private mongoService: MongoService) { }
+
+  ngOnInit() {
+    this.mongoService.getJobList().subscribe(res => this.jobs = res);
+  }
+
+}
--- src/app/services/mongo.service.ts
+++ src/app/services/mongo.service.ts
@@ -7,12 +7,17 @@ import {TESTDATA} from '../mock-data';
 
 @Injectable()
 export class MongoService {
-
+  private baseUrl = 'http://localhost:3000/api/';
   constructor(private http: HttpClient) { }
 
   getFailsafeReports(jobName: string, buildId: string): Observable<FailsafeReport[]> {
-    const url = `http://localhost:3000/api/failsafereports/${jobName}/${buildId}`;
+    const url = `${this.baseUrl}failsafereports/${jobName}/${buildId}`;
     return this.http.get<FailsafeReport[]>(url);
+  }
+
+  getJobList(): Observable<String[]> {
+    const url = `${this.baseUrl}joblist`;
+    return this.http.get<String[]>(url);
   }
 
   // getFailsafeReports(jobName: string, buildId: string): Observable<FailsafeReport[]> {
