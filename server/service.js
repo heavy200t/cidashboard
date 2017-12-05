016b3e3b32abe77eeb51e9b860b418cb68a1e375
panbin <panbinhd@gmail.com>
2017-12-05 14:13:53 +0800

    Add mail template

--- server/service.js
+++ server/service.js
@@ -1,3 +1,46 @@
+var mock_data = {summary: [
+    {jobName: 'job1', buildId: 12, total: 120, pass: 23, fail: 97},
+    {jobName: 'job2', buildId: 34, total: 30, pass: 23, fail: 7},
+  ],
+  newFailed: [
+    {
+      testClassName: "com.hp.automation.configuration.platform.ems.importFromFile.EMSImportMultiTenancyFlows",
+      testName: "run"
+    },
+    {
+      testClassName: "com.hp.automation.configuration.platform.ems.importFromFile.EMSImportMultiTenancyFlows",
+      testName: "testIt"
+    }
+  ],
+  top5: [
+    {
+      testClassName: "com.hp.automation.configuration.platform.ems.importFromFile.EMSImportMultiTenancyFlows",
+      testName: "run",
+      age: 58
+    },
+    {
+      testClassName: "com.hp.automation.configuration.platform.ems.importFromFile.EMSImportMultiTenancyFlows",
+      testName: "testIt",
+      age: 49
+    },
+    {
+      testClassName: "com.hp.automation.junit.search.topics.TopicsExtractionServiceTestAcc",
+      testName: "testQNA",
+      age: 30
+    },
+    {
+      testClassName: "com.hp.automation.junit.search.topics.TopicsExtractionServiceTestAcc",
+      testName: "testGetTopicsCache",
+      age: 9
+    },
+    {
+      testClassName: "com.hp.automation.junit.search.topics.TopicsExtractionServiceTestAcc",
+      testName: "testAdHocEms",
+      age: 1
+    },
+  ],
+};
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var mongoClient = require('mongodb');
@@ -58,7 +101,7 @@ app.get('/api/jobs', function (req, res) {
 });
 
 app.get('/testmail', function(req, res){
-  res.send(compileFun({name: 'panbin'}));
+  res.send(compileFun(mock_data));
 });
 
 app.get('/api/:jobName/builds', function (req, res) {
