6d62f57ade51b9616e01b428364856848469f7c5
panbin <panbinhd@gmail.com>
2017-12-15 15:19:18 +0800

    screen capture

--- server/capturePage.js
+++ server/capturePage.js
@@ -1,6 +1,22 @@
-let page = require('webpage').create();
-let today = new Date();
-page.open('http://localhost:4200/dailyReport', function() {
-  page.render('DailyReport_'+today.getFullYear()+today.getMonth()+today.getDate()+'.png');
-  phantom.exit();
-});
+const {Builder, By, Key, until}  = require('selenium-webdriver');
+
+
+var driver = new Builder()
+  .forBrowser('firefox')
+  .build();
+
+driver.get('http://shc-devops-master.hpeswlab.net:30080/dailyReport')
+// driver.get('http://localhost:4200/dashboard')
+  .then(_ => driver.findElement(By.tagName('app-root')))
+  .then(_ => driver.takeScreenshot().then(
+    function (image,err) {
+      require('fs').writeFile('test.png', image, 'base64', function (err) {
+        console.log(err);
+      })
+    }
+  ))
+  .then(_ => driver.close());
+// driver.takeScreenshot().then(
+
+//   );
+
--- server/mailTest.js
+++ server/mailTest.js
@@ -6,7 +6,7 @@ const mongoClient = require('mongodb');
 const DB_CONN_STR = 'mongodb://shc-devops-master.hpeswlab.net:27017/failsafereports';
 const data = [];
 
-const sendMail = function(data) {
+const sendMail = function() {
   nodemailer.createTestAccount((err, account) => {
     let transporter = nodemailer.createTransport({
       host: 'smtp3.hpe.com',
@@ -14,13 +14,13 @@ const sendMail = function(data) {
     });
 
     let mailOptions = {
-      from: 'test@hpe.com',
-      to: 'bpan@hpe.com',
+      from: 'bpan@hpe.com',
+      to: 'liangwei.yu@hpe.com, meic@hpe.com,bpan@hpe.com',
       subject: 'test mail',
       html: compileFunction(mock_data),
       attachments:[{
         filename: 'image001.png',
-        path: 'templates/image001.png',
+        path: 'templates/Dailyreport_20171215.png',
         contentTpe: 'image/png',
         contentDisposition: "inline",
         cid: '0000001'
@@ -37,26 +37,6 @@ const sendMail = function(data) {
   });
 };
 
-const getJobSummary = function(db, jobName, buildId) {
-  db.collection('reports').aggregate([
-      {$match: {"jobName": jobName, "buildId": buildId}},
-      {$group: {
-        _id: {category: "$category", reportUrl: "$testReportUrl"},
-        total: {$sum: 1},
-        pass: {$sum: {$cond: {if: "$testFailed", then: 0, else: 1}}},
-        fail: {$sum: {$cond: {if: "$testFailed", then: 1, else: 0}}},
-        unstable: {$sum: {$cond: {if: "$markedUnstable", then: 1, else: 0}}}
-      }}
-    ],
-    function(err,result) {
-      data.append({"summary": result});
-      db.close();
-    }
-  );
-};
-
-mongoClient.connect(DB_CONN_STR, function (err,db) {
-  getJobSummary(db, 'ITSMA-PR-X', 1029);
-});
+sendMail();
 
 // console.info(getJobSummary('ITSMA-PR-X', 1029));
