69488773a8d20385b52816d7c13289c622a5aa07
panbin <bpan@hpe.com>
2017-12-18 21:48:04 +0800

    capture screenshot

--- server/capturePage.js
+++ server/capturePage.js
@@ -1,15 +1,16 @@
 const {Builder, By, Key, until, Capabilities}  = require('selenium-webdriver');
+const nodemailer = require('nodemailer');
+const pug = require('pug');
+const compileFunction = pug.compileFile('../templates/mail.pug');
 
-// var customPhantom = Capabilities.phantomjs();
-// customPhantom.set('/usr/bin/phantomjs');
-// var driver = new Builder().
-//   withCapabilities(customPhantom).
-//   build();
+const SELENIUM_HUB = 'http://localhost:32768/wd/hub';
+const URL_BASE = 'http://shc-devops-master.hpeswlab.net/dailyReport/';
+
 var pageWidth = 0;
 var pageHeight = 0;
 var driver = new Builder()
   .forBrowser('chrome')
-  .usingServer('http://shc-selenium-hub.hpeswlab.net:4444/wd/hub')
+  .usingServer(SELENIUM_HUB)
   .build();
 
 function sleep(delay) {
@@ -20,12 +21,47 @@ function sleep(delay) {
   }
 }
 
+const sendMail = function(fileName, url, cid) {
+  nodemailer.createTestAccount((err, account) => {
+    let transporter = nodemailer.createTransport({
+      service: 'qq',
+      auth: {
+        user: '3716743@qq.com',
+        pass: 'ondxxgkwiieucafc' //授权码,通过QQ获取
+
+      }
+    });
+
+    let mailOptions = {
+      from: '3716743@qq.com',
+      to: 'bpan@hpe.com',
+      subject: 'test mail',
+      html: compileFunction({"url": url, "cid": cid}),
+      attachments:[{
+        filename: 'image001.png',
+        path: './' + fileName,
+        contentTpe: 'image/png',
+        contentDisposition: "inline",
+        cid: cid
+      }]
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        return console.log(error);
+      }
+      console.log('Message sent: %s', info.message);
+      console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    });
+  });
+};
+
 let today = new Date();
 let yesterday = new Date(today.setDate(today.getDate() - 1));
 let str_yesterday = yesterday.getFullYear() + '-' + (yesterday.getMonth() + 1).toString() + '-' + yesterday.getDate();
-let url = 'http://shc-devops-master.hpeswlab.net:30080/dailyReport/' + str_yesterday;
+let url = URL_BASE + str_yesterday;
 let screenshotFileName = 'dailyReport_' + str_yesterday + '.png';
-
+console.log(url);
 driver.get(url)
   .then(_ => driver.findElement(By.tagName('app-daily-report')))
   .then(sleep(5000))
@@ -50,6 +86,7 @@ driver.get(url)
       });
     }
   )
-  .then(_ => driver.close());
+  .then(_ => driver.close())
+  .then(_ => sendMail(screenshotFileName, url, str_yesterday));
 
 
