b11041b7f37b6fc8469aae815e396439f7af51f2
panbin <panbinhd@gmail.com>
2018-01-11 16:13:57 +0800

    Remove ag-grid, change to dx-data grid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,10 @@ import { TestResultComponent } from './test-result/test-result.component';
 import { AppRoutingModule } from './app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import {HttpClientModule} from '@angular/common/http';
-import {AgGridModule} from 'ag-grid-angular';
 import { JobListComponent } from './job-list/job-list.component';
 import { BuildListComponent } from './build-list/build-list.component';
 import {AccordionModule, ProgressbarModule} from 'ngx-bootstrap';
-import { DailyReportComponent } from './daily-report/daily-report.component';
-import {DxChartModule, DxLinearGaugeModule, DxPieChartModule} from 'devextreme-angular';
-import { LinkComponent } from './daily-report/link/link.component';
+import {DxChartModule, DxDataGridModule, DxLinearGaugeModule, DxPieChartModule} from 'devextreme-angular';
 
 
 
@@ -24,16 +21,14 @@ import { LinkComponent } from './daily-report/link/link.component';
     TestResultComponent,
     DashboardComponent,
     JobListComponent,
-    BuildListComponent,
-    DailyReportComponent,
-    LinkComponent,
+    BuildListComponent
   ],
   imports: [
     BrowserModule,
     DxPieChartModule,
     DxLinearGaugeModule,
+    DxDataGridModule,
     DxChartModule,
-    AgGridModule.withComponents([LinkComponent]),
     AccordionModule.forRoot(),
     ProgressbarModule.forRoot(),
     HttpClientModule,
--- src/app/test-result/test-result.component.ts
+++ src/app/test-result/test-result.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import {MongoService} from '../services/mongo.service';
 import {ActivatedRoute} from '@angular/router';
 import {DailyReportJob} from '../data-model/daily-report-job';
-import {LinkComponent} from "../daily-report/link/link.component";
 
 @Component({
   selector: 'app-test-result',
@@ -12,28 +11,13 @@ import {LinkComponent} from "../daily-report/link/link.component";
 export class TestResultComponent implements OnInit {
   jobName: string;
   buildId: string;
-  columnDefs = [
-    { headerName: 'Category',
-      width: 420,
-      valueGetter: function(params) {
-        return {'category': params.data.combinedCategory, 'reportUrl': params.data._id.reportUrl}; },
-      cellRendererFramework: LinkComponent},
-    {headerName: 'Total', width: 160, field: 'total'},
-    {headerName: 'Pass', width: 160, valueGetter: function(params) {
-      const data = params.data;
-      return data.pass + '(' + data.pass_percent  + '%)';
-    }},
-    {headerName: 'Fail', width: 160, valueGetter: function(params) {
-      const data = params.data;
-      return data.fail + '(' + data.fail_percent + '%)';
-    }},
-    {headerName: 'Unstable', width: 160, valueGetter: function(params) {
-      const data = params.data;
-      return data.unstable + '(' + data.unstable_percent + '%)';
-    }}
-  ];
 
   result: DailyReportJob;
+
+  combinedPass(data) {return [data.pass, '(', data.pass_percent, '%)'].join(''); }
+  combinedFail(data) {return [data.fail, '(', data.fail_percent, '%)'].join(''); }
+  combinedUnstable(data) {return [data.unstable, '(', data.unstable_percent, '%)'].join(''); }
+  categoryWithLink(data) {return {'link': data._id.reportUrl, 'category': data.combinedCategory}; }
 
   constructor(private mongoService: MongoService, private route: ActivatedRoute) {}
 
